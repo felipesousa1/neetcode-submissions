aee65da7c67e2cc7b661bc8a075c93fe5a82ef07
Felipe Sousa dos Santos <106843344+felipesousa1@users.noreply.github.com>
2026-08-08 21:47:00 -0300

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    vector<string> res;
+    void genPar(string str, int open, int close, int n){
+        if (close > open || open > n) return;
+        if (open==n && close==n){
+            res.push_back(str);
+            return;
+        } 
+        genPar(str+'(', open+1, close, n);
+        genPar(str+')', open, close+1, n);
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        genPar("", 0, 0, n);
+
+        return res;
+    }
+};
