bbe190a17c73a119a7ff22f00c2c4d3f410b00cf
Felipe Sousa dos Santos <106843344+felipesousa1@users.noreply.github.com>
2026-08-19 21:31:44 -0300

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private: 
+    int res = 0;
+    void validPal(const string& s, int l, int r){
+        int n = s.size();
+        while(l>=0 && r < n && s[l] == s[r]){
+            res++;
+            l--; r++;
+        }
+
+    }
+public:
+    int countSubstrings(string s) {
+        for (int i = 0; i < s.size(); i++){
+            validPal(s, i, i);
+            validPal(s, i, i+1);
+        }
+
+        return res;
+    }
+};
